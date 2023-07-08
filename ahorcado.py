a68985a9f7505cd97c263efb32a4f1eaf0cf0340
vanessa2493 <56240421+vanessa2493@users.noreply.github.com>
2023-07-08 08:56:15 -0300

    First Commit

---
+++ ahorcado.py
@@ -0,0 +1,13 @@
+ ## Libraries##
+import random
+
+
+ ## Variables ##
+# List of words the user can access to play
+words = ["aeropuerto", "hospital", "programadora", "saturno", "escaleras"]
+# The number of attemps the user has
+life_count = 5
+# The list in which the wrong letters per attempt would be added and shown to the user
+wrong_letters = []
+# The list in which the right letters per attemp would be added and showed to the user
+right_letters = []
